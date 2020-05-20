8dff071aa8f0bb330c9de8d2372989cf6da127c1
Rusty Tanton <rusty.tanton@gmail.com>
2020-05-20 13:18:19 -0400

    move wordpress test install script to provision folder

--- bin/bootstrap-tests.sh
+++ bin/bootstrap-tests.sh
@@ -1,5 +1,6 @@
 SCRIPT_DIR=$( dirname "${BASH_SOURCE[0]}" )
 
 if [ -x "${SCRIPT_DIR}/docker-compose-check.sh" ]; then
-    docker-compose run --rm wordpress-phpunit /app/install-wp-tests.sh wordpress_test root '' mysql-phpunit latest true
+    # docker-compose run --rm wordpress-phpunit /app/install-wp-tests.sh wordpress_test root '' mysql-phpunit latest true
+    docker-compose run --rm wordpress-phpunit /usr/local/bin/install-wp-tests.sh wordpress_test root '' mysql-phpunit latest true
 fi
