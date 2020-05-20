62f33091327d866e258eaf9448dba630ff3bef4f
Rusty Tanton <rusty.tanton@gmail.com>
2020-05-20 16:09:46 -0400

    improve test runner formatting

--- bin/test-runner.sh
+++ bin/test-runner.sh
@@ -1,8 +1,11 @@
 SCRIPT_DIR=$( dirname "${BASH_SOURCE[0]}" )
+KILL_MSG="Press Ctrl + C to stop"
 
 if [ -x "$(command -v fswatch)" ]; then
+    clear
     echo "PHPUnit test runner started..."
-    fswatch -r -e ".*" -i "\\.php$" ./src | (while read; do ${SCRIPT_DIR}/phpunit.sh; done)
+    echo $KILL_MSG
+    fswatch -r -e ".*" -i "\\.php$" ./src | (while read; do clear && ${SCRIPT_DIR}/phpunit.sh --colors=always && echo $KILL_MSG; done)
 else
     echo "\n\nTest runner requires fswatch utility...\n\n"\
 "See: https://github.com/emcrisostomo/fswatch\n\n"
