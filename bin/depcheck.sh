6614163ebd57fd4e393c68e6aef1c38f396d6167
Rusty Tanton <rusty.tanton@gmail.com>
2020-05-26 10:48:00 -0400

    cleanup/improve depcheck.sh

--- bin/depcheck.sh
+++ bin/depcheck.sh
@@ -1,5 +1,5 @@
-if ! [ -x "$(docker-compose)" ]; then
-    echo "\n\n--\nYou need to install docker-compose to use this script...\n\n"\
+if ! [ -x "$(command -v docker-compose)" ]; then
+    echo "\n\n--\nScript requires docker-compose\n\n"\
 "On Mac / Windows install Docker Desktop, or if you have Homebrew on a Mac try:\n\n"\
 "brew install docker docker-machine docker-compose\n"\
 "brew cask install virtualbox\n"\
@@ -8,14 +8,24 @@ if ! [ -x "$(docker-compose)" ]; then
     exit 1
 fi
 
-if ! [ -x "${fswatch}" ]; then
-    echo "\n\nTest runner requires fswatch utility...\n\n"\
-"See: https://github.com/emcrisostomo/fswatch\n\n"
+if ! [ -x "$(command -v fswatch)" ]; then
+    echo "\n\n--\nScript requires fswatch utility\n\n"\
+"See: https://github.com/emcrisostomo/fswatch\n--\n\n"
     exit 1
 fi
 
-if ! [ -x "${realpath}" ]; then
-    echo "\n\nYou need the realpath utility. On Mac OS try:\n"
-"brew install coreutils"
+if ! [ -x "$(command -v realpath)" ]; then
+    echo "\n\n--\nScript requires realpath utility\n\nIf you have Homebrew on a Mac, try:\n"\
+"brew install coreutils\n--\n\n"
     exit 1
+fi
+
+# Platform-specific
+if [[ $(uname) == "Darwin" ]]; then # MacOS
+    if ! [ -x "$(command -v docker-machinee)" ]; then
+        echo "\n\n--\ndocker-machine required on Mac OS\n"\
+"If you have Homebrew try:\n
+brew install docker-machine\n--\n\n"
+    exit 1
+    fi
 fi
